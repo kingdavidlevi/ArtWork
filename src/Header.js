d12f50375ba62d17e97854a09e746bffc397c66f
king david levi <125862163+kingdavidlevi@users.noreply.github.com>
2024-07-13 18:21:44 +0100

    updated

--- src/Header.js
+++ src/Header.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
 
 function Header() {
   const [isVisible, setIsVisible] = useState(false);
@@ -13,31 +14,17 @@ function Header() {
       <div className="w-full fixed z-10 glass-header  h-16 flex place-items-center justify-between pr-3 md:pr-12">
         <div className="text-white">LOGO HERE</div>
         <div className="flex gap-5 md:gap-6">
-          <button className="md:px-8 text-white hover:bg-red-400 bg-red-500 py-2  md:text-base text-sm px-4 font-medium rounded-lg">
-            Login
-          </button>
-          <button className="md:px-6 text-white hover:bg-purple-500 bg-purple-600 py-2 px-3 md:text-base text-sm font-medium rounded-lg">
-            Sign Up
-          </button>
-        </div>
-      </div>
-      <div className="picture md:h-250 h-68 grid place-content-center w-full ">
-        {' '}
-        <h1 className="font-bold  sm:text-8xl text-6xl md:text-9xl lg:text-12xl nft-font text-white">
-          Artify Nft's
-        </h1>
-        <p className="text-yellow-400 font-bold mt-4 md:mt-8 md:text-3xl grid place-items-center ">
-          Market limitless outstanding nft
-        </p>
-      </div>
-      <div className=" mt-6 paragraph-container bg-black py-2">
-        <div className=" sliding-paragraph">
-          <p className="text-gray-400 text-lg font-semibold md:text-xl md:font-bold">
-            Welcome to our NFT minting site! Enjoy seamless minting with low
-            fees and user-friendly interfaces. Our platform ensures top-notch
-            security for your digital assets. Experience fast transactions,
-            detailed analytics, and personalized support.
-          </p>
+          <NavLink to="/Login">
+            {' '}
+            <button className="md:px-8 text-white hover:bg-red-400 bg-red-500 py-2  md:text-base text-sm px-4 font-medium rounded-lg">
+              Login
+            </button>
+          </NavLink>
+          <NavLink to="/SignUp">
+            <button className="md:px-6 text-white hover:bg-purple-500 bg-purple-600 py-2 px-3 md:text-base text-sm font-medium rounded-lg">
+              Sign Up
+            </button>
+          </NavLink>
         </div>
       </div>
     </section>
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,9 +1,33 @@
 import React from 'react';
 import Header from './Header';
+import { FaCommentDots } from 'react-icons/fa';
 function Homepage() {
   return (
     <div>
       <Header />
+      <div className="picture md:h-250 h-68 grid place-content-center w-full ">
+        {' '}
+        <h1 className="font-bold  sm:text-8xl text-6xl md:text-9xl lg:text-12xl nft-font text-white">
+          Artify Nft's
+        </h1>
+        <p className="text-yellow-400 font-bold mt-4 md:mt-8 md:text-3xl grid place-items-center ">
+          Market limitless outstanding nft
+        </p>
+      </div>
+      <div className=" mt-6 paragraph-container bg-black py-2">
+        <div className=" sliding-paragraph">
+          <p className="text-gray-300 text-lg font-semibold md:text-xl md:font-bold">
+            Welcome to our NFT minting site! Enjoy seamless minting with low
+            fees and user-friendly interfaces. Our platform ensures top-notch
+            security for your digital assets. Experience fast transactions,
+            detailed analytics, and personalized support.
+          </p>
+        </div>
+      </div>
+      <div className="w-12 bottom-5 h-12 z-10  grid border right-5 absolute border-blue-200 place-content-center rounded-full bg-blue-500">
+        {' '}
+        <FaCommentDots className="text-blue-100 w-6 h-6" />
+      </div>
     </div>
   );
 }
