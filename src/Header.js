6a55d692aee64201e0fe447f5dcc0cb855dd0381
king david levi <125862163+kingdavidlevi@users.noreply.github.com>
2024-07-13 17:27:55 +0100

    updated

--- src/Header.js
+++ src/Header.js
@@ -10,7 +10,7 @@ function Header() {
   }, []);
   return (
     <section>
-      <div className="w-full fixed z-10 bg-blue-900 h-16 flex place-items-center justify-between pr-3 md:pr-12">
+      <div className="w-full fixed z-10 glass-header  h-16 flex place-items-center justify-between pr-3 md:pr-12">
         <div className="text-white">LOGO HERE</div>
         <div className="flex gap-5 md:gap-6">
           <button className="md:px-8 text-white hover:bg-red-400 bg-red-500 py-2  md:text-base text-sm px-4 font-medium rounded-lg">
@@ -26,11 +26,11 @@ function Header() {
         <h1 className="font-bold  sm:text-8xl text-6xl md:text-9xl lg:text-12xl nft-font text-white">
           Artify Nft's
         </h1>
-        <p className="text-white font-bold mt-4 md:mt-8 md:text-3xl grid place-items-center ">
+        <p className="text-yellow-400 font-bold mt-4 md:mt-8 md:text-3xl grid place-items-center ">
           Market limitless outstanding nft
         </p>
       </div>
-      <div className="paragraph-container bg-black py-2">
+      <div className=" mt-6 paragraph-container bg-black py-2">
         <div className=" sliding-paragraph">
           <p className="text-gray-400 text-lg font-semibold md:text-xl md:font-bold">
             Welcome to our NFT minting site! Enjoy seamless minting with low
