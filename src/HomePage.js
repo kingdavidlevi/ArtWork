c62457c730c51d3a1dfae17346903bc5142601ec
king david levi <125862163+kingdavidlevi@users.noreply.github.com>
2024-07-29 14:28:08 +0100

    upgraded

--- src/HomePage.js
+++ src/HomePage.js
@@ -3,7 +3,13 @@ import Header from './Header';
 import Collection from './Collection';
 import { useOutletContext } from 'react-router-dom';
 
-import { FaGreaterThan, FaCalendar, FaCommentDots } from 'react-icons/fa6';
+import {
+  FaGreaterThan,
+  FaCalendar,
+  FaCommentDots,
+  FaUsers,
+  FaPencil,
+} from 'react-icons/fa6';
 function Homepage() {
   const { isOpen, setIsOpen } = useOutletContext();
   return (
@@ -21,7 +27,28 @@ function Homepage() {
             <p className="text-white  text-base font-semibold">Drops</p>
           </li>
           <li>
-            <FaGreaterThan className="font-bold text-sm text-white" />
+            <FaGreaterThan className="font-bold text-lg text-white" />
+          </li>
+        </ul>
+        <ul className="flex px-4  text-red-600  cursor-pointer justify-end w-full ul items-center  h-20 ">
+          <li className="flex  items-center gap-3 w-full">
+            <FaPencil className="text-white  text-2xl" />
+            <p className="text-white  text-base font-semibold">Create</p>
+          </li>
+        </ul>
+        <ul className="flex px-4  text-red-600  cursor-pointer justify-end w-full ul items-center  h-20 ">
+          <li className="flex  items-center gap-3 w-full">
+            <FaUsers className="text-white  text-3xl" />
+            <p className="text-white  text-base font-semibold">Authors</p>
+          </li>
+        </ul>
+        <ul className="flex px-4  text-red-600  cursor-pointer justify-end w-full ul items-center  h-20 ">
+          <li className="flex  items-center gap-3 w-full">
+            <FaCalendar className="text-white  text-2xl" />
+            <p className="text-white  text-base font-semibold"></p>
+          </li>
+          <li>
+            <FaGreaterThan className="font-bold text-lg text-white" />
           </li>
         </ul>
       </div>
