7b07b5ad4510c73c16a2ae21f0f7fece54ce09ec
king david levi <125862163+kingdavidlevi@users.noreply.github.com>
2024-10-07 17:30:13 +0100

    up

--- src/UsersChat.js
+++ src/UsersChat.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { FaTimes } from 'react-icons/fa';
+import { FaTimes, FaRegPaperPlane } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 /* // Reset height to auto
  */
@@ -10,7 +10,12 @@ function UserChat({ openchat, setOpenChat }) {
     { text: 'Hello!', isSelf: false },
     { text: 'Hi there!', isSelf: true },
     { text: 'How are you?', isSelf: false },
+    { text: 'How are you?', isSelf: false },
     { text: 'I am good, thank you!', isSelf: true },
+    {
+      text: 'I am good, thank you! i want to mint an art how do i do it',
+      isSelf: true,
+    },
   ]);
   const navigate = useNavigate();
   const handleChange = (event) => {
@@ -27,11 +32,12 @@ function UserChat({ openchat, setOpenChat }) {
   };
 
   return (
-    <div className="md:h-80% md:w-97 text-area  overflow-y-scroll pt-300   md:rounded-xl md:right-6 h-full z-30 w-full dropdown shadow-xl fixed bottom-0  md:bottom-6">
+    <div className="md:h-80% md:w-97 text-area pt-28 overflow-y-scroll    md:rounded-xl md:right-6 h-full z-30 w-full dropdown shadow-xl fixed bottom-0  md:bottom-6">
       <div className="h-32 pl-10 fixed w-full z-50 top-0 md:top-24 md:w-97 md:rounded-t-xl pt-3 bg-blue-600">
         <h1 className="text-white text-lg font-medium">
           Hello <span>David</span> {'\u{1F44B}'},
         </h1>
+        <p></p>
         <h1 className="text-white mt-4 text-lg font-medium">
           How can we help you?
         </h1>
@@ -53,13 +59,13 @@ function UserChat({ openchat, setOpenChat }) {
         </div>
       </div>
 
-      <section className="fixed md:block hidden  pl-4  z-40 bottom-4 w-full md:bottom-20">
+      <section className="fixed md:block  md:w-97 pr-4  pl-4  z-40 bottom-4 w-full md:bottom-20">
         <form>
           <textarea
             value={text}
             rows="1"
             onChange={handleChange}
-            className=" pl-4 pr-14 text-area overflow-y-scroll block h-auto max-h-32 md:w-86 w-75 placeholder:text-base placeholder:font-normal text-base font-medium  outline-none rounded-2xl py-1.5 bg-white"
+            className=" pl-4 pr-14 text-area overflow-y-scroll block h-auto max-h-32 w-90%  placeholder:text-base placeholder:font-normal text-base font-medium  outline-none rounded-2xl py-1.5 bg-white"
             placeholder="Type your message here"
             style={{
               // Hide the scrollbar
@@ -69,39 +75,39 @@ function UserChat({ openchat, setOpenChat }) {
             }}
           />
         </form>
+        <div className="h-10 grid place-items-center cursor-pointer fixed right-2 md:right-8 bottom-4 md:bottom-20 rounded-full text-white bg-blue-600 w-10">
+          <svg
+            fill="#fff"
+            width="20px"
+            height="20px"
+            viewBox="0 0 32 32"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>paper-plane-top</title>
+            <path d="M31.083 16.589c0.105-0.167 0.167-0.371 0.167-0.589s-0.062-0.421-0.17-0.593l0.003 0.005c-0.030-0.051-0.059-0.094-0.091-0.135l0.002 0.003c-0.1-0.137-0.223-0.251-0.366-0.336l-0.006-0.003c-0.025-0.015-0.037-0.045-0.064-0.058l-28-14c-0.163-0.083-0.355-0.132-0.558-0.132-0.691 0-1.25 0.56-1.25 1.25 0 0.178 0.037 0.347 0.104 0.5l-0.003-0.008 5.789 13.508-5.789 13.508c-0.064 0.145-0.101 0.314-0.101 0.492 0 0.69 0.56 1.25 1.25 1.25 0 0 0 0 0.001 0h-0c0.001 0 0.002 0 0.003 0 0.203 0 0.394-0.049 0.563-0.136l-0.007 0.003 28-13.999c0.027-0.013 0.038-0.043 0.064-0.058 0.148-0.088 0.272-0.202 0.369-0.336l0.002-0.004c0.030-0.038 0.060-0.082 0.086-0.127l0.003-0.006zM4.493 4.645l20.212 10.105h-15.88zM8.825 17.25h15.88l-20.212 10.105z"></path>
+          </svg>
+        </div>
       </section>
-      <section className="md:hidden  pl-4  z-40 bottom-4 w-full md:bottom-20">
-        <form>
-          <textarea
-            value={text}
-            rows="1"
-            onChange={handleChange}
-            className=" pl-4 pr-14 text-area overflow-y-scroll block h-auto max-h-32 md:w-86 w-75 placeholder:text-base placeholder:font-normal text-base font-medium  outline-none rounded-2xl py-1.5 bg-white"
-            placeholder="Type your message here"
-            style={{
-              // Hide the scrollbar
-              resize: 'none', // Disable manual resizing
-              boxSizing: 'border-box',
-              lineHeight: '1.5em',
-            }}
-          />
-        </form>
-      </section>
-      <div className="h-300  text-white mt-40 ">
+
+      <div className="h-300  text-white mx-6 ">
         {messages.map((message, index) => (
           <div
             key={index}
-            className={`message-wrapper ${message.isSelf ? 'left' : 'right'}`}
+            className={`message-wrapper  font-medium ${
+              message.isSelf ? 'left' : 'right'
+            }`}
           >
             <div
-              className={`chat-message ${
-                message.isSelf ? 'right bg-blue-600' : 'left'
+              className={`chat-message  ${
+                message.isSelf ? 'right ' : 'left  '
               }`}
             >
               {message.text}
             </div>
           </div>
         ))}
+        <div className="text-white">haaaa</div>
       </div>
     </div>
   );
